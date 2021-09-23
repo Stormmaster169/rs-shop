ee65acfaa3c933592a8deafdbfe79cc9b2010079
Stormmaster169 <Stormmaster169@gmail.com>
2021-09-23 23:32:37 +0300

    feat: added unit tests

--- rs-shop/src/app/app.component.spec.ts
+++ rs-shop/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('rs-shop');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rs-shop app is running!');
-  });
 });
