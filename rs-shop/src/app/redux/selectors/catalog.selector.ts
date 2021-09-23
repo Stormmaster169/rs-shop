05c196321dfa5408a8cb2fd97d95096708b16af7
Stormmaster169 <Stormmaster169@gmail.com>
2021-09-23 21:47:22 +0300

    feat: add new functionality

--- rs-shop/src/app/redux/selectors/catalog.selector.ts
+++
@@ -1,5 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { ICatalogState } from '../store/catalog.state';
-
-export const catalogFeatureSelector = createFeatureSelector<ICatalogState>('catalog');
-export const catalogResultSelector = createSelector(catalogFeatureSelector, (state) => state.data);
--- rs-shop/src/app/redux/store/state.ts
+++
@@ -1,8 +0,0 @@
-import { ICatalogState } from "./catalog.state";
-import { IUserState } from "./user.state";
-
-
-export interface IAppState {
-  catalog: ICatalogState,
-  user: IUserState
-}
